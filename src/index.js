82121d6dc5cda4a1c40b0ad541d59fa681ac3a0c
henhal <henhalvor@gmail.com>
2023-08-29 21:12:13 +0200

    add initialLoad function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import UI from "./modules/ui";
+import Storage from "./modules/storage";
 
-UI.loadHomepage();
+function initialLoad() {
+    // Load Data
+    Storage.loadDataFromStorage(Storage.allTasks);
+    Storage.loadDataFromStorage(Storage.allLists);
+    Storage.loadDataFromStorage(Storage.allTags);
+    
+    // Load homePage
+    UI.loadHomepage();
+}
+
+initialLoad();
